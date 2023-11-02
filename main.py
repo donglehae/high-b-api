4a291d6f6c0a24b16f94e02a2183ea340a1ee6ae
donglehae <dongledh@gmail.com>
2023-11-03 01:05:59 +0900

    feat(router): main.py 파일 수정 및 user router 추가

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
+from routers import user
 
 app = FastAPI()
+
+routers = [user.router]
+for router in routers: app.include_router(router)
 
 if __name__ == "__main__":
   uvicorn.run(app, host="0.0.0.0", port=30180)
