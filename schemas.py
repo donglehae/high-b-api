af76cacc17ad227c2060170a72dcd4b7899d8bb5
donglehae <dongledh@gmail.com>
2023-11-10 01:25:57 +0900

    feat(schemas): 병원 및 영업시간 관련 스키마 정의

--- schemas.py
+++ schemas.py
@@ -16,9 +16,42 @@ class UserBase(BaseModel):
   class Config:
     from_attributes = True
     
+class BusinessHourBase(BaseModel):
+  mon: Optional[str] = None
+  tue: Optional[str] = None
+  wed: Optional[str] = None
+  thu: Optional[str] = None
+  fri: Optional[str] = None
+  sat: Optional[str] = None
+  sun: Optional[str] = None
+  class Config:
+    from_attributes = True
+
+class HospitalBase(BaseModel):
+  name: str
+  address: str
+  introduction: Optional[str] = None
+  class Config:
+    from_attributes = True
+    
 # 사용자 스키마
 class UserCreate(UserBase):
   password: str
   
 class User(UserBase):
   id: int
+  
+# 영업시간 스키마
+class BusinessHour(BusinessHourBase):
+  pass
+  
+# 병원 스키마
+class HospitalCreate(HospitalBase):
+  pass
+
+class Hospital(HospitalBase):
+  image: Optional[str] = None
+  rating: float
+
+class HospitalDetail(Hospital):
+  business_hour: BusinessHour
